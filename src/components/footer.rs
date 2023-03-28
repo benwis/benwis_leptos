49248918df7e3eedfe1e039a8de16a1b93fbd5b1
benwis <ben@celcyon.com>
2023-03-28 14:14:47 -0700

    It's not me, it's Fly

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -13,16 +13,16 @@ pub fn Footer(cx: Scope) -> impl IntoView{
 
     let mut navigation: Vec<FooterLink> = Vec::new();
 
-    // navigation.push(FooterLink{
-    //     name: "RSS",
-    //     href: "/rss.xml",
-    //     icon: r#"
-    //         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-    //             <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-    //         </svg>
-    //     "#,
-    //     rel: None
-    // });
+    navigation.push(FooterLink{
+        name: "RSS",
+        href: "/rss.xml",
+        icon: r#"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+        "#,
+        rel: None
+    });
     navigation.push(FooterLink{
         name: "Twitter",
         href: "https://twitter.com/iambenwis",
