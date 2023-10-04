5d29fb807fbcbac53a54ca5b308f7339b9f4c71b
benwis <ben@benw.is>
2023-10-04 12:33:11 -0700

    Added experimental islands features, chopped file size by a lot

--- src/lib.rs
+++ src/lib.rs
@@ -26,10 +26,8 @@ cfg_if! {
         pub fn hydrate() {
             _ = console_log::init_with_level(log::Level::Info);
             console_error_panic_hook::set_once();
+            leptos::leptos_dom::HydrationCtx::stop_hydrating();
 
-            leptos::mount_to_body(|| {
-                view! {   <BenwisApp/> }
-            });
         }
     }
 }
